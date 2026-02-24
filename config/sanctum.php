82aadaaad16379cbfb70d3b10f8486b921725c61
Nantenaina29 <elhadari04@gmail.com>
2026-02-24 06:45:08 +0300

    Update sanctum stateful domains

--- config/sanctum.php
+++ config/sanctum.php
@@ -4,7 +4,7 @@ use Laravel\Sanctum\Sanctum;
 
 return [
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5173,127.0.0.1:5173')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5173,127.0.0.1:5173,tsinjoainafianara.vercel.app')),
 
 
     'guard' => ['web'],
