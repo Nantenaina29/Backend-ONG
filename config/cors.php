50bc6cb882ec4a76f4ff024640eb44b885d6d184
Nantenaina29 <elhadari04@gmail.com>
2026-02-24 13:20:21 +0300

    Fix: CORS and origins for Vercel

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://tsinjoainafianara.vercel.app'], 
+    'allowed_origins' => ['https://tsinjoainafianara.vercel.app', 'https://frontend-474qoanrl-nantenaina29s-projects.vercel.app'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // TENA ILAINA: Ity no mamela ny Cookie
+    'supports_credentials' => true, 
 ];
