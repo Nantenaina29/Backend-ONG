817f2de53f6c72ab9f7a68325956f20cf2cd138b
Nantenaina29 <elhadari04@gmail.com>
2026-02-25 20:06:46 +0300

    fix: modification CORS

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@ return [
     'paths' => ['api/*', 'sanctum/*', 'sanctum/csrf-cookie', 'login', 'register'], 
      
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://frontend-ong-puce.vercel.app'], 
+    'allowed_origins' => ['https://frontend-ong-puce.vercel.app', 'frontend-ong-puce.vercel.app' ], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
     'max_age' => 0,
     'supports_credentials' => true, 
 ];
